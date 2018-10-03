41e2de115100ece2c615ca3cfe4c04346ebdce95
Tony Varlamov <tonywarlamoff@gmail.com>
2018-10-03 23:10:25 +0300

    complete work with db

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # __name__ - переопределённая переменная python,
 # задаётся именем модуля, в котором используется
@@ -9,6 +10,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 # Импорт в конце чтобы избежать циклического импорта
 from app import routes, models
